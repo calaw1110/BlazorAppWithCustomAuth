24b24cacd98ea602eb7f99607a249a0b70d531db
calaw1110 <calaw1110@gmail.com>
2024-10-23 22:41:35 +0800

    加入專案檔案。

---
+++ BlazorAppWithCustomAuth/Auth/UserAccountService.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorAppWithCustomAuth.Auth
+{
+    public class UserAccountService
+    {
+        private List<UserAccount> _users;
+        public UserAccountService()
+        {
+            _users = new List<UserAccount>()
+            {
+                new UserAccount() {UserName="admin",Password="123",Role="ADMIN" },
+                new UserAccount() {UserName="user",Password="123",Role="USER" },
+            };
+        }
+
+        public UserAccount? GetByUserName(string userName)
+        {
+            return _users.Find(x => x.UserName == userName);
+        }
+    }
+}
---
+++ BlazorAppWithCustomAuth/Auth/UserSession.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorAppWithCustomAuth.Auth
+{
+    public class UserSession
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+    }
+}
